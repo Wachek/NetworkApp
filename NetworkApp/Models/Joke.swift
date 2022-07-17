917e9b1cd8d5da4f8873217ee811053450e5aa3f
Viacheslav Turchak <wachek7@gmail.com>
2022-07-17 22:21:45 +0300

    setup joke fetch

---
+++ NetworkApp/Models/Joke.swift
@@ -0,0 +1,11 @@
+//
+//  Joke.swift
+//  NetworkApp
+//
+//  Created by Вячеслав Турчак on 17.07.2022.
+//
+
+struct Joke: Decodable {
+    let setup: String
+    let delivery: String
+}
--- NetworkApp/ViewControllers/ViewController.swift
+++ NetworkApp/ViewControllers/ViewController.swift
@@ -9,11 +9,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dogPictureView: UIImageView!
+    @IBOutlet var jokeLabel: UILabel!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
+        
     }
 
+    @IBAction func pictureButtonPressed() {
+        dogPictureView.isHidden = false
+        jokeLabel.text = ""
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        fetchImage()
+    }
+    
+    @IBAction func jokeButtonPressed() {
+        dogPictureView.isHidden = true
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        fetchJoke()
+    }
+    
+    private func fetchImage() {
+        guard let url = URL(string: "https://random.dog/c22c077e-a009-486f-834c-a19edcc36a17.jpg") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                self.dogPictureView.image = image
+                self.activityIndicator.stopAnimating()
+            }
+        }.resume()
+    }
+    
+    private func fetchJoke() {
+        guard let url = URL(string: "https://v2.jokeapi.dev/joke/Any?safe-mode") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
 
+            do {
+                let joke = try JSONDecoder().decode(Joke.self, from: data)
+                DispatchQueue.main.async {
+                    self.jokeLabel.text = "\(joke.setup)\n\(joke.delivery)"
+                    self.activityIndicator.stopAnimating()
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+            
+        }.resume()
+    }
+    
 }
 
